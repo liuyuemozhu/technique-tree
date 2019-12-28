fed2ed85d69933af95f489a0eea8f35a0fb1edaa
liuyuemozhu <319917926@qq.com>
2019-12-28 18:34:50 +0800

    增加comment-demo（评论小练习）

---
+++ comment-demo/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header-container">
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      msg: "请发表对Vue的评论"
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+$header-height: 100px;
+$header-color: #ccc;
+
+.header-container {
+  height: $header-height;
+  background: $header-color;
+  h2 {
+    line-height: $header-height;
+    text-align: center;
+  }
+}
+</style>
